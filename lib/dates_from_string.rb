3cc08df02d8a260f6d9ec957a2a3012766e8aa04
Sergey <sergey@2net.me>
2015-04-27 18:23:48 +0300

    add new stuff

--- lib/dates_from_string.rb
+++ lib/dates_from_string.rb
@@ -1,9 +1,11 @@
 require "dates_from_string/version"
+require "pry-rails"
 
 class DatesFromString
 
-  def initialize
-
+  def initialize(key_words = [])
+    @main_arr =[]
+    @key_words = key_words
   end
 
   def find_date(sructure)
@@ -11,6 +13,56 @@ class DatesFromString
   end
 
   def get_structure(string)
+    data_arr = string.split(" ")
+    @indexs = []
 
+    data_arr.each_with_index do |data, index|
+      set_structura
+      value = get_year(data)
+      next_index = index + 1
+
+      if value
+        @structura[:type] = :year
+        @structura[:value] = value
+      end
+
+      if value && @main_arr.last
+        @main_arr.last[:distance] = calc_index(index)
+      end
+
+      if @key_words.any? && @key_words.include?(data_arr[next_index])
+        @structura[:key_words] << data_arr[next_index]
+      end
+
+      if value
+        @main_arr <<  @structura
+        value = nil
+      end
+
+    end
+
+    return @main_arr
+  end
+
+  def get_year(string)
+    if string =~ /^\d{4}$/
+      string
+    else
+      nil
+    end
+  end
+
+  def calc_index(index)
+    result = nil
+    @indexs << index
+    if @indexs.count > 1
+      result = (index - @indexs[-2])
+    else
+      result = index
+    end
+  end
+
+  def set_structura
+    @structura = {type: nil, value: nil, distance: nil, key_words: []}
   end
 end
--- spec/dates_from_string_spec.rb
+++ spec/dates_from_string_spec.rb
@@ -3,13 +3,46 @@ require 'spec_helper'
 describe DatesFromString do
   subject { DatesFromString.new }
 
-  # it 'has a version number' do
-  #   expect(DatesFromString::VERSION).not_to be nil
-  # end
+  describe '#get_structure' do
 
-  it 'get date structure from string' do
-    input = 'he was born in 1988'
-    output = [[:year,'1988',nil,nil]]
-    expect(subject.get_structure(input)).to eq(output)
+    it 'find one year' do
+      input = 'he was born in 1990'
+      output = [{:type=>:year, :value=>"1990", :distance=>nil, :key_words=>[]}]
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find two year' do
+      input = '1990 1988 year of his probably birthday'
+      output = [
+        {:type=>:year, :value=>"1990", :distance=>1, :key_words=>[]},
+        {:type=>:year, :value=>"1988", :distance=>nil, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find many year and many distance' do
+      input = '1990 1988 year 2013 bla bla bla 2015 a b c d i f g qwe 2016'
+      output = [
+        {:type=>:year, :value=>"1990", :distance=>1, :key_words=>[]},
+        {:type=>:year, :value=>"1988", :distance=>2, :key_words=>[]},
+        {:type=>:year, :value=>"2013", :distance=>4, :key_words=>[]},
+        {:type=>:year, :value=>"2015", :distance=>9, :key_words=>[]},
+        {:type=>:year, :value=>"2016", :distance=>nil, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date year month and day format one' do
+      input = '2015-02-02'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>nil, :key_words=>[]},
+        {:type=>:month, :value=>"02", :distance=>nil, :key_words=>[]},
+        {:type=>:day, :value=>"01", :distance=>nil, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
   end
 end
