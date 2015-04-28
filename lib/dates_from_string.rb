13dcb3e28363d1475678e6a691ad6738ea2e51ab
Sergey <sergey@2net.me>
2015-04-28 17:03:40 +0300

    add new type of dates structure

--- lib/dates_from_string.rb
+++ lib/dates_from_string.rb
@@ -17,28 +17,25 @@ class DatesFromString
     @indexs = []
 
     data_arr.each_with_index do |data, index|
-      set_structura
-      value = get_year(data)
+      value_year = get_year(data)
+      value_full_date = get_full_date(data)
+      value_dash = get_dash_data(data)
       next_index = index + 1
 
-      if value
-        @structura[:type] = :year
-        @structura[:value] = value
+      if value_year
+        add_to_structure(:year ,value_year, index, next_index, data_arr)
       end
 
-      if value && @main_arr.last
-        @main_arr.last[:distance] = calc_index(index)
+      if value_full_date
+        add_to_structure(:year ,value_full_date[0], index, next_index, data_arr)
+        add_to_structure(:month ,value_full_date[1], index, next_index, data_arr)
+        add_to_structure(:day ,value_full_date[2], index, next_index, data_arr)
       end
 
-      if @key_words.any? && @key_words.include?(data_arr[next_index])
-        @structura[:key_words] << data_arr[next_index]
+      if value_dash
+        add_to_structure(:year ,value_dash[0], index, next_index, data_arr, '-')
+        add_to_structure(:year ,value_dash[1], index, next_index, data_arr)
       end
-
-      if value
-        @main_arr <<  @structura
-        value = nil
-      end
-
     end
 
     return @main_arr
@@ -49,6 +46,57 @@ class DatesFromString
       string
     else
       nil
+    end
+  end
+
+  def get_full_date(string)
+    if string =~ (/\d{4}-\d{2}-\d{2}/)
+      string.split("-")
+    elsif string =~ (/\d{2}-\d{2}-\d{4}/)
+      string.split("-").reverse
+    elsif string =~ (/\d{4}\.\d{2}\.\d{2}/)
+      string.split(".")
+    elsif string =~ (/\d{2}\.\d{2}\.\d{4}/)
+      string.split(".").reverse
+    elsif string =~ (/\d{4}\/\d{2}\/\d{2}/)
+      string.split("/")
+    elsif string =~ (/\d{2}\/\d{2}\/\d{4}/)
+      string.split("/").reverse
+    else
+      nil
+    end
+  end
+
+  def get_dash_data(string)
+    if string =~ (/\d{4}-\d{4}/)
+      string.split("-")
+    else
+      nil
+    end
+  end
+
+  def add_to_structure (type ,value, index, next_index, data_arr, key_word = nil)
+    set_structura
+    if value
+      @structura[:type] = type
+      @structura[:value] = value
+    end
+
+    if value && @main_arr.last
+      @main_arr.last[:distance] = calc_index(index)
+    end
+
+    if @key_words && @key_words.include?(data_arr[next_index])
+      @structura[:key_words] << data_arr[next_index]
+    end
+
+    if key_word
+      @structura[:key_words] << key_word
+    end
+
+    if value
+      @main_arr <<  @structura
+      value = nil
     end
   end
 
@@ -63,6 +111,6 @@ class DatesFromString
   end
 
   def set_structura
-    @structura = {type: nil, value: nil, distance: nil, key_words: []}
+    @structura = {type: nil, value: nil, distance: 0, key_words: []}
   end
 end
--- spec/dates_from_string_spec.rb
+++ spec/dates_from_string_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe DatesFromString do
-  subject { DatesFromString.new }
+  subject { DatesFromString.new(['between','-']) }
 
   describe '#get_structure' do
 
     it 'find one year' do
       input = 'he was born in 1990'
-      output = [{:type=>:year, :value=>"1990", :distance=>nil, :key_words=>[]}]
+      output = [{:type=>:year, :value=>"1990", :distance=>0, :key_words=>[]}]
       expect(subject.get_structure(input)).to eq(output)
     end
 
@@ -15,7 +15,7 @@ describe DatesFromString do
       input = '1990 1988 year of his probably birthday'
       output = [
         {:type=>:year, :value=>"1990", :distance=>1, :key_words=>[]},
-        {:type=>:year, :value=>"1988", :distance=>nil, :key_words=>[]}
+        {:type=>:year, :value=>"1988", :distance=>0, :key_words=>[]}
       ]
 
       expect(subject.get_structure(input)).to eq(output)
@@ -28,7 +28,7 @@ describe DatesFromString do
         {:type=>:year, :value=>"1988", :distance=>2, :key_words=>[]},
         {:type=>:year, :value=>"2013", :distance=>4, :key_words=>[]},
         {:type=>:year, :value=>"2015", :distance=>9, :key_words=>[]},
-        {:type=>:year, :value=>"2016", :distance=>nil, :key_words=>[]}
+        {:type=>:year, :value=>"2016", :distance=>0, :key_words=>[]}
       ]
 
       expect(subject.get_structure(input)).to eq(output)
@@ -37,12 +37,179 @@ describe DatesFromString do
     it 'find full date year month and day format one' do
       input = '2015-02-02'
       output = [
-        {:type=>:year, :value=>"2015", :distance=>nil, :key_words=>[]},
-        {:type=>:month, :value=>"02", :distance=>nil, :key_words=>[]},
-        {:type=>:day, :value=>"01", :distance=>nil, :key_words=>[]}
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"02", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"02", :distance=>0, :key_words=>[]}
       ]
 
       expect(subject.get_structure(input)).to eq(output)
     end
+
+    it 'find full date year month and day format two' do
+      input = '2015.02.02'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"02", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"02", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date year month and day format three' do
+      input = '02-02-2015'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"02", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"02", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date year month and day format four' do
+      input = '02.02.2015'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"02", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"02", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date year month and day format five' do
+      input = '12/07/2014'
+      output = [
+        {:type=>:year, :value=>"2014", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"07", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"12", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date year month and day format six' do
+      input = '2013/07/09'
+      output = [
+        {:type=>:year, :value=>"2013", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"07", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"09", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find full date and year ' do
+      input = '2013/07/09 one date and he born in 1990'
+      output = [
+        {:type=>:year, :value=>"2013", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"07", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"09", :distance=>7, :key_words=>[]},
+        {:type=>:year, :value=>"1990", :distance=>0, :key_words=>[]}
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find short date one' do
+      input = '00/00/2015 one year'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"00", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"00", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find short date two' do
+      input = '00/10/2015 one year'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"10", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"00", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find short date three' do
+      input = '2015/10/00 one year'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"10", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"00", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'find short date four' do
+      input = '2015/00/00 one year'
+      output = [
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"00", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"00", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'year and special word between' do
+      input = '1988 between 1990'
+      output = [
+        {:type=>:year, :value=>"1988", :distance=>2, :key_words=>['between']},
+        {:type=>:year, :value=>"1990", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'year and special sympol' do
+      input = '1988 - 1990'
+      output = [
+        {:type=>:year, :value=>"1988", :distance=>2, :key_words=>['-']},
+        {:type=>:year, :value=>"1990", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'year and special sympol other way' do
+      input = '1988-1990'
+      output = [
+        {:type=>:year, :value=>"1988", :distance=>0, :key_words=>['-']},
+        {:type=>:year, :value=>"1990", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'year and special sympol other way and one more year' do
+      input = '1988-1990 and 2000'
+      output = [
+        {:type=>:year, :value=>"1988", :distance=>0, :key_words=>['-']},
+        {:type=>:year, :value=>"1990", :distance=>2, :key_words=>[]},
+        {:type=>:year, :value=>"2000", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
+    it 'year and special sympol other way and year and full date' do
+      input = '1988-1990 and 2000 and one more date 28.04.2015'
+      output = [
+        {:type=>:year, :value=>"1988", :distance=>0, :key_words=>['-']},
+        {:type=>:year, :value=>"1990", :distance=>2, :key_words=>[]},
+        {:type=>:year, :value=>"2000", :distance=>5, :key_words=>[]},
+        {:type=>:year, :value=>"2015", :distance=>0, :key_words=>[]},
+        {:type=>:month, :value=>"04", :distance=>0, :key_words=>[]},
+        {:type=>:day, :value=>"28", :distance=>0, :key_words=>[]},
+      ]
+
+      expect(subject.get_structure(input)).to eq(output)
+    end
+
   end
 end
