772c3b7d653df4ef13a9567b449690fccb2564a6
Sergey <sergey@2net.me>
2015-04-29 11:35:32 +0300

    rollback binding

--- lib/dates_from_string.rb
+++ lib/dates_from_string.rb
@@ -55,7 +55,6 @@ class DatesFromString
   end
 
   def get_full_date(string)
-    binding.pry
     if string =~ (/\d{4}-\d{2}-\d{2}/)
       string.split("-")
     elsif string =~ (/\d{2}-\d{2}-\d{4}/)
@@ -68,9 +67,8 @@ class DatesFromString
       string.split("/")
     elsif string =~ (/\d{2}\/\d{2}\/\d{4}/)
       string.split("/").reverse
-    elsif string =~(/\d{2}\s{1}(Jan|Feb|Mar|Apr|May|Jun|Jul|Apr|Sep|Oct|Nov|Dec)\s{1}\d{4}/)
-      binding.pry
-      string.to_date.to_s.split("-")
+    # elsif string =~(/\d{2}\s{1}(Jan|Feb|Mar|Apr|May|Jun|Jul|Apr|Sep|Oct|Nov|Dec)\s{1}\d{4}/)
+    #   string.to_date.to_s.split("-")
     else
       nil
     end
